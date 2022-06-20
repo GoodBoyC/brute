d0356aa89ef3be0d5c6fe7e7cb0988b8b0c6bac8
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2022-06-20 19:33:32 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,23 @@
+import os
+try:
+    import requests
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul requests belum terinstall!...\n')
+    os.system('pip install requests')
+
+try:
+    import bs4
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Bs4 belum terinstall!...\n')
+    os.system('pip install bs4')
+
+try:
+    import rich
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
+    os.system('pip install rich')
+#################################################################################
+from src import cok
+
+if __name__ == '__main__':
+    cok.Brute().cek_xxx()
