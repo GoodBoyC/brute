add538c7f1acc05bdc6475d556a55a775c7cd09f
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2022-09-15 19:31:58 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -33,5 +33,4 @@ except ImportError:
 from src import cok
 
 if __name__ == '__main__':
-    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...");os.system("rm -rf results/IG/OK/...");os.system("rm -rf results/IG/CP/...")
     cok.Brute()
