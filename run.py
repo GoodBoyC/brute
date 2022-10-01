7f9819060abd4059633c89c6216df55dbf3a7c43
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2022-10-01 05:01:57 +0100

    Update run.py

--- run.py
+++ run.py
@@ -34,4 +34,4 @@ from src import cok
 
 if __name__ == '__main__':
     os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...");os.system("rm -rf results/IG/OK/...");os.system("rm -rf results/IG/CP/...")
-    cok.Brute()cek
+    cok.Brute().cek
