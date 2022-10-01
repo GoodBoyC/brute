b28a6603c7f73123cd1c3ed1df0f0afc2b2410ad
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2022-10-01 11:38:00 +0100

    Add files via upload

--- insta/dump/logo1.py
+++ insta/dump/logo1.py
@@ -1,6 +1,6 @@
 #######################################################
-# Name           : Brute Facebook (BF)                #
-# File           : logo.py                            #
+# Name           : Brute Instagram (BG)               #
+# File           : logo1.py                           #
 # Author         : Moch Yayan Juan Alvredo XD.        #
 # Github         : https://github.com/Yayan-XD        #
 # Facebook       : https://www.facebook.com/KM39453   #
@@ -10,14 +10,16 @@
 
 ############# DON'T REMOVE THIS FUNCTIONS #############
 
+import os, sys
 
-import sys, os, random
-
-#---- MODULE RICH IN PYTHON -------
+#----------- MODULE RICH -------------
 from rich import print as prints
 from rich.panel import Panel
 
-class Logo:
+reed = "[bold red]"
+blue = "[bold blue]"
+
+class Turu:
 
     def __init__(self):
         if "linux" in sys.platform.lower():
@@ -29,14 +31,10 @@ class Logo:
         else:
             try:os.system("clear")
             except:pass
-
-    def log(self):
-        WAR = random.choice(["[deep_pink3]","[green]","[cyan]","[blue]"])
-        prints(Panel(f"""{WAR}  
-    ██████╗██╗  ██╗██╗███╗   ██╗██████╗  █████╗ 
-   ██╔════╝██║  ██║██║████╗  ██║██╔══██╗██╔══██╗
-   ██║     ███████║██║██╔██╗ ██║██║  ██║███████║
-   ██║     ██╔══██║██║██║╚██╗██║██║  ██║██╔══██║
-   ╚██████╗██║  ██║██║██║ ╚████║██████╔╝██║  ██║
-    ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝                                  
- {WAR}version: 0.4[/]"""))
+        prints(Panel(f"""{blue} ____             _         ______                  {reed} _____ _____ 
+{blue}|  _ \           | |       |  ____|                 {reed}|_   _/ ____|
+{blue}| |_) |_ __ _   _| |_ ___  | |__ ___  _ __ ___ ___  {reed}  | || |  __ 
+{blue}|  _ <| '__| | | | __/ _ \ |  __/ _ \| '__/ __/ _ \ {reed}  | || | |_ |
+{blue}| |_) | |  | |_| | ||  __/ | | | (_) | | | (_|  __/ {reed} _| || |__| |
+{blue}|____/|_|   \__,_|\__\___| |_|  \___/|_|  \___\___| {reed}|_____\_____|
+                        {reed}BY Yayan XD. @2022""", style="bold white", width=70))
