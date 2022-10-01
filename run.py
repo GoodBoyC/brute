6527a738e5614b4d250e74bf31b8aed534a361a3
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2022-10-01 10:53:38 +0100

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ if bit == '64bit':
 elif bit == '32bit':
     print("Not work on 32bit")
 
+from src import cok
 if __name__ == '__main__':
     os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...");os.system("rm -rf results/IG/OK/...");os.system("rm -rf results/IG/CP/...")
     cok.Brute()
